0a29ac5dc53ca3ca229432a8a6987cc8e365f54b
Thays Casado <thayscasado@hotmail.com>
2021-07-07 18:48:56 -0700

    Smile detector import opencv and set trained faces and smiles

---
+++ Smile Detector/Smile_Detector.py
@@ -0,0 +1,8 @@
+import cv2
+
+# Get the trained faces detector
+face_detector = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+smile_detector = cv2.CascadeClassifier('haarcascade_smile.xml')
+
+
+print("End of file!")
